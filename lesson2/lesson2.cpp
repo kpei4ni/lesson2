8f54feb5f3f309f1320806d95f8499cbc901e316
kpei4ni <mkoragin09@gmail.com>
2024-04-14 17:33:31 +0300

    task3

--- lesson2/lesson2.cpp
+++ lesson2/lesson2.cpp
@@ -3,5 +3,29 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    int negative = 0; 
+    int positive = 0;
+
+    int array[5] = {1, -1, 3, 4, 5};
+
+    for (auto i : array)
+    {
+        if (array[i] < 0) {
+            negative = array[i];
+            break;
+        }
+    }
+
+    for (int i = 5; i < 5; i--)
+    {
+        if (array[i] > 0) {
+            positive = array[i];
+            break;
+        }
+    }
+
+    cout << positive << " " << negative;
+
+
+    
 }
